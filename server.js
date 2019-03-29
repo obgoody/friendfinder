9d3e559091ca9441e53af5cf424ca0df58e0a70d
obgoody <obgoody@gmail.com>
2019-03-29 14:58:29 -0700

    server.js add

---
+++ server.js
@@ -0,0 +1,34 @@
+// Load express module
+const express = require("express");
+
+// Load path module  https://nodejs.org/api/path.html
+const path = require("path");
+
+// Load chalk module
+const chalk = require("chalk");
+
+// Setup Express
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
