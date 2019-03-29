66a15e7426dddf99994f55637ac0ad739fdcbfad
obgoody <obgoody@gmail.com>
2019-03-29 14:59:24 -0700

    apiRoute,js add

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,30 @@
+// Your `apiRoutes.js` file should contain two routes:
+//    * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
+//    * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+
+
+// Load express module
+const express = require("express");
+
+// Load path module  https://nodejs.org/api/path.html
+const path = require("path");
+
+module.exports = (app) => {
+    // API GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases the user is shown an HTML page of content
+    // ---------------------------------------------------------------------------
+  
+    // Route that leads to the survey page
+    app.get("/api/friends", (req, res) => {
+        res.send(`get api/friends ${JSON.stringify(req)}`);
+    });
+
+
+    // A default, catch-all route that leads to `home.html` which displays the home page.
+    app.post("/api/friends", (req, res) => {
+        res.send(`post api/friends ${req.data}`);
+    });
+
+
+};
