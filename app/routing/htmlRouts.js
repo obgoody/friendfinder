7be424aa60a3d7c2523a20ffcf40e15903b0bf3a
obgoody <obgoody@gmail.com>
2019-03-29 15:00:25 -0700

    htmlRoute.js add

---
+++ app/routing/htmlRouts.js
@@ -0,0 +1,31 @@
+// 3. Your `htmlRoutes.js` file should include two routes:
+
+//    * A GET Route to `/survey` which should display the survey page.
+//    * A default, catch-all route that leads to `home.html` which displays the home page.
+
+
+// Load express module
+const express = require("express");
+
+// Load path module  https://nodejs.org/api/path.html
+const path = require("path");
+
+module.exports = (app) => {
+    // HTML GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases the user is shown an HTML page of content
+    // ---------------------------------------------------------------------------
+  
+    // Route that leads to the survey page
+    app.get("/survey", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+
+
+    // A default, catch-all route that leads to `home.html` which displays the home page.
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+
+
+};
