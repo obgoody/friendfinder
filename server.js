925aa441ee5ed18f1f840d242b3bc8c2b9a5c618
obgoody <obgoody@gmail.com>
2019-03-31 14:24:33 -0700

    parser

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Load express module
 const express = require("express");
-
+const bodyParser = require("body-parser");
 // Load path module  https://nodejs.org/api/path.html
 const path = require("path");
 
@@ -12,8 +12,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.static(__dirname + "/app/css"));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
 
 // ================================================================================
 // ROUTER
